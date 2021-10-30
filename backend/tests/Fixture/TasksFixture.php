ef67d16ba5ed69833da31116c417f2bd4772b9c1
ktplato <k.y.93.0210@gmail.com>
2021-10-30 17:07:29 +0900

    bake model Tasks

---
+++ backend/tests/Fixture/TasksFixture.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * TasksFixture
+ */
+class TasksFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => '77784128-2129-48e1-a872-3d4de9fcf46b',
+                'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+                'created' => '2021-10-30 03:21:09',
+                'modified' => '2021-10-30 03:21:09',
+            ],
+        ];
+        parent::init();
+    }
+}
